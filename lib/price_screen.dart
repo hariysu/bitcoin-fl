6ded1f078afad96272c70604b8edad24be6a8992
Abdurahman AYDIN <hariysu@gmail.com>
2020-07-07 14:52:12 +0300

    Includes price screen

---
+++ lib/price_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'coin_data.dart';
+import 'dart:io' show Platform;
+import 'coin_card.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'AUD';
+
+  DropdownButton<String> androidDropdown() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropdownItems.add(newItem);
+    }
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: dropdownItems,
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value;
+          getData();
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(currency));
+    }
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        selectedCurrency = currenciesList[selectedIndex];
+        getData();
+      },
+      children: pickerItems,
+    );
+  }
+
+  String bitcoin1Value = '?';
+  String bitcoin2Value = '?';
+  String bitcoin3Value = '?';
+
+  void getData() async {
+    try {
+      List<double> data = await CoinData().getCoinData(selectedCurrency);
+      setState(() {
+        bitcoin1Value = data[0].toStringAsFixed(0);
+        bitcoin2Value = data[1].toStringAsFixed(0);
+        bitcoin3Value = data[2].toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('🤑 Coin Ticker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          CoinCard(
+            bitcoinValue: bitcoin1Value,
+            selectedCurrency: selectedCurrency,
+            whichCoin: 'BTC',
+          ),
+          CoinCard(
+            bitcoinValue: bitcoin2Value,
+            selectedCurrency: selectedCurrency,
+            whichCoin: 'ETH',
+          ),
+          CoinCard(
+            bitcoinValue: bitcoin3Value,
+            selectedCurrency: selectedCurrency,
+            whichCoin: 'LTC',
+          ),
+          SizedBox(
+            height: 216.0,
+          ),
+          Container(
+            height: 150.0,
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 30.0),
+            color: Colors.lightBlue,
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
+          ),
+        ],
+      ),
+    );
+  }
+}
